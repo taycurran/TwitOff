5b498de856b6f926e1636bdc2c38b39339a3628f
taycurran <taylorcurranagtc@gmail.com>
2020-02-24 17:54:03 -0600

    Added Example Users and Tweets

--- TwitOff/app.py
+++ TwitOff/app.py
@@ -1,11 +1,17 @@
 """Code for our app"""
 
 from flask import Flask
-
+from .models import DB
 # Make our App Factory
 
 def create_app():
     app = Flask(__name__)
+
+    # Add config for database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+    # Have the database know about the app
+    DB.init_app(app)
 
     @app.route("/")
     def root():
