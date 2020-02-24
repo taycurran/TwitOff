9ec5aaa6b07afa36781b347456229fc3adead723
taycurran <taylorcurranagtc@gmail.com>
2020-02-24 16:45:35 -0600

    adding html templates folder and file

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 """Minimal Flask App"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 # Make the Application
 app = Flask(__name__)
@@ -10,5 +10,13 @@ app = Flask(__name__)
 
 # Now Define a Function
 
-def hello:
+def hello():
     return "Hello World!"
+
+# Make a 2nd Route
+
+@app.route("/about")
+
+# Now Make Function that Goes with About
+def preds():
+    return render_template('about.html')
