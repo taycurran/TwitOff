a02b08c28acb20712a5381e4027d5791ed21ec42
taycurran <taylorcurranagtc@gmail.com>
2020-02-25 12:03:06 -0600

    Created Templates Folder and Started Base HTML Template

--- TwitOff/models.py
+++ TwitOff/models.py
@@ -6,10 +6,15 @@ class User(DB.Model):
     """Twitter users that we analyze"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 class Tweet(DB.Model):
     """Tweets we pull"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(300))
-    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id')), nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    def __repr__(self):
+        return '<User {}>'.format(self.text)
